9a7a3d7b39dca364180302b7f6f4efc6b4429a1d
HarshaVardhanHR <57298115+HarshaVardhanHR@users.noreply.github.com>
2022-11-06 19:06:27 +0530

    Inital push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  questions = [
+    {
+      id:1,
+     q:'What is a question?',
+     a:'A question is a type of sentence that is asking for an anwer. Questions end with the question mark symbol.',
+     category_name:'Category 1'},
+     {
+       id:2,
+     q:'What makes a good question?',
+     a:'A good question is one where the answer is not clear or obvious.',
+     category_name:'Category 2'},
+     {
+       id:3,
+     q:'What is a rhetorical question?',
+     a:'A rhetorical question is a type of question that answers the question.',
+     category_name:'Category 1'}
+   ];
+ 
 }
