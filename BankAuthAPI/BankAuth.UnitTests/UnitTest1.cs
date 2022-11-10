45c99d2bf738976e32652c060f87154be75bcd42
Ishaan Ohri <ishaanohri@192.168.1.7>
2022-11-10 21:21:12 +0530

    Add all unit tests

--- BankAuthAPI/BankAuth.UnitTests/UnitTest1.cs
+++ BankAuthAPI/BankAuth.UnitTests/UnitTest1.cs
@@ -1,25 +1,68 @@
-﻿using BankAuthAPI.Models;
+﻿using BankAuthAPI.Context;
+using BankAuthAPI.Helper;
+using BankAuthAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BankAuth.UnitTests;
 
 [TestFixture]
-public class Tests
+public class UserTests
 {
+    private readonly AppDbContext _authContext;
+
     [SetUp]
     public void Setup()
     {
+        // Add DB connection here
     }
 
     [Test]
-    public void Test1()
+    public async Task InvalidUsername_ReturnsFalse()
+    {
+        // Arrange
+        var user = new User();
+
+        // Act
+        user.Username = "new.user";
+        var result = await _authContext.Users.AnyAsync(x => x.Username == user.Username);
+
+        // Assert
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public async Task CorrectCredentials_ReturnsTrue()
     {
         // Arrange
         var user = new User();
 
         // Act
         user.Username = "test.user";
+        user.Password = "abc@123";
+
+        var dbUser = await _authContext.Users.FirstOrDefaultAsync(x => x.Username == user.Username);
+
+        var result = PasswordHasher.VerifyPassword(dbUser.Password, user.Password);
 
         // Assert
-        Assert.That(false);
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public async Task InvalidPassword_ReturnsFalse()
+    {
+        // Arrange
+        var user = new User();
+
+        // Act
+        user.Username = "test.user";
+        user.Password = "xyz@123";
+
+        var dbUser = await _authContext.Users.FirstOrDefaultAsync(x => x.Username == user.Username);
+
+        var result = PasswordHasher.VerifyPassword(dbUser.Password, user.Password);
+
+        // Assert
+        Assert.That(result, Is.False);
     }
 }
