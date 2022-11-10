ff7c15a5799064d664bb2a78232ce674cddb4461
Ishaan Ohri <ishaanohri@192.168.1.7>
2022-11-10 17:18:23 +0530

    Add 3 unit tests

---
+++ BankAuthAPI/BankAuth.UnitTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using BankAuthAPI.Models;
+
+namespace BankAuth.UnitTests;
+
+[TestFixture]
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        // Arrange
+        var user = new User();
+
+        // Act
+        user.Username = "test.user";
+
+        // Assert
+        Assert.That(false);
+    }
+}
